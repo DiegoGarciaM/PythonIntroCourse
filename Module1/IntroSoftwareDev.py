d26c3075ddaa96fbe57096112de9f5c0ab7c6462
Diego Garcia Martin <garcia.martin.diego.mail@gmail.com>
2023-11-02 16:27:12 -0400

    started intro to software dev

--- Module1/IntroSoftwareDev.py
+++ Module1/IntroSoftwareDev.py
@@ -1,1 +1,22 @@
 # Introduccion al desarrollo de software
+
+# Simplemente es el proceso de crear software, lo cual puede darse de muchas maneras
+# Podemos crear scripts sencillos para tareas sencillas, juegos, analizar datos,
+# crear paginas web, servcios completos, APIs, programas de escritorio, etc.
+
+# Al final se trata de crear y editar archivos que la computadora va a interpretar
+# Una vez que comprendemos eso, nos damos cuenta de que no importa mucho el lenguaje
+# Eso siempre se puede cambiar, los conceptos basicos son los mismos
+
+# Lo importante es saber los conceptos basicos, para todo lo demas tenemos google
+# El problema es que no puedo googlear algo que ni siquiera se que existe
+# Conocer los conceptos me hace poder buscarlos, y lo que use con regularidad lo recordare
+
+# Tambien es importante entender que mucho del proceso es exploratorio
+# No se trata de escribir codigo correcto a la primera, si puedes hacerlo el codigo es muy sencillo
+# Es normal ir por partes, es normal crear y luego editar, es normal hacer pruebas poco a poco
+
+# A veces no queremos escribir algo que vamos a borrar, o ir poco a poco
+# Eso nos hace recurrir demasiado a tutoriales, y a copiar y pegar codigo
+# No tiene nada de malo hacerlo de vez en cuando, pero si no aceptamos el proceso mas exploratorio
+# Nunca podremos crear nuestro propio codigo y nuestros propios proyectos
